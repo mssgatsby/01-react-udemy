87541c58fd9db91fe02bad5e4729642883b05b23
mssgatsby <mssgatsby@gmail.com>
2023-12-04 16:33:52 +0500

    Add new lines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const variables = ["Fundamentals", "Core", "Crucial"];
+
 function genRandom(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function App() {
@@ -19,7 +31,19 @@ function App() {
         </p>
       </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The core UI building blocks."
+              image={componentsImg}
+            />
+            <CoreConcepts title="Props" />
+            <CoreConcepts />
+            <CoreConcepts />
+          </ul>
+        </section>
       </main>
     </div>
   );
