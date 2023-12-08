cc5abe57096526c110883346ad287ebafaebf812
mssgatsby <mssgatsby@gmail.com>
2023-12-08 11:44:23 +0500

    Add tab content

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
-    console.log(selectedButton);
-    console.log(selectedTopic);
+    // console.log(selectedButton);
+    // console.log(selectedTopic);
   }
 
   return (
@@ -41,7 +42,13 @@ function App() {
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
